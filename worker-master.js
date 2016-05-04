592c3d207e44c3ccec32c966d9b6c0102f4df025
redaphid <iam@aaronherres.com>
2016-05-03 21:56:05 -0700

    worker-master listens for work, but does nothing about it

--- worker-master.js
+++ worker-master.js
@@ -1,18 +1,68 @@
-var Redis = require('ioredis');
-var uuid = require('uuid')
+'use strict'
+var _         = require('lodash')
+var Redis     = require('ioredis');
+var dashdash  = require('dashdash');
 
-var emitterClient = new Redis();
-var masterClient = new Redis();
-var workerClient = new Redis();
+class PFMWorkerMaster {
+  constructor(subscribeTopic, queue) {
+      this.subscribeTopic = subscribeTopic;
+      this.queue = queue;
+  }
 
-masterClient.subscribe('point', function(){
-  console.log('subscribed to point');
-  masterClient.on('message', function(channel, message){
-      console.log('got', message);
-  });
+  start() {
+    console.log('starting up');
+    this.client = new Redis();
+    this.client.subscribe(this.subscribeTopic);
+    this.client.on('message', (channel, message) => this.fillQueue(message));
+  }
 
-  var data = uuid.v4()
-  console.log(`emitting to point: ${data}`)
-  emitterClient.publish('point', data)
+  fillQueue (message) {
+    const jobs = JSON.parse(message);
+    _.map(jobs, (job) => this.queueJob(job));
+  }
 
-})
+  queueJob (job) {
+      console.log(`I was going to queue up ${job}, but I didn't.`);
+  }
+}
+
+const options = doCliStuff();
+
+const master = new PFMWorkerMaster(options.subscribeTopic, options.queue);
+
+master.start();
+
+function doCliStuff() {
+  var cliParser = dashdash.createParser({options: [
+    {
+      names: ['subscribe-topic', 's'],
+      type: 'string',
+      help: 'the name of the topic to subscribe'
+    },
+    {
+      names: ['queue', 'q'],
+      type: 'string',
+      help: 'the name of the work queue to fill'
+    }
+  ]});
+
+  var options = {}
+  try {
+    options = cliParser.parse(process.argv);
+  }
+  catch(error) {
+    die();
+  }
+
+  if(!options.subscribe_topic || !options.queue) {
+    die();
+  }
+
+  function die() {
+    console.error("You did something wrong.")
+    console.error(cliParser.help());
+    process.exit(-1);
+  }
+
+  return {queue: options.queue, subscribeTopic: options.subscribe_topic};
+}
