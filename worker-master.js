28d273dfceeb35b10e5c27385096bc574f99b2ad
redaphid <iam@aaronherres.com>
2016-05-03 22:21:21 -0700

    working on worker. Using debug, so I can feel fancy

--- worker-master.js
+++ worker-master.js
@@ -2,24 +2,27 @@
 var _         = require('lodash')
 var Redis     = require('ioredis');
 var dashdash  = require('dashdash');
+var debug     = require('debug')('pfm:worker-master');
 
 class PFMWorkerMaster {
-  constructor(subscribeTopic, queue) {
-      this.subscribeTopic = subscribeTopic;
+  constructor(beginTopic, queue) {
+      this.beginTopic = beginTopic;
       this.queue = queue;
   }
 
   start() {
-    console.log('starting up');
+    debug(`Listening on ${this.beginTopic} to add work to ${this.queue}`)
     this.client = new Redis();
     this.subscriber = new Redis();
-    this.subscriber.subscribe(this.subscribeTopic);
+    this.subscriber.subscribe(this.beginTopic);
     this.subscriber.on('message', (channel, message) => this.fillQueue(message));
   }
 
   fillQueue (message) {
     const jobs = JSON.parse(message);
-    console.log(`putting ${jobs.length} jobs into ${this.queue}`);
+
+    debug(`putting ${jobs.length} jobs into ${this.queue}`);
+
     this.client.lpush(this.queue, _.map(jobs, JSON.stringify))
       .then( ()=> this.getEmAll() )
   }
@@ -27,8 +30,11 @@ class PFMWorkerMaster {
   getEmAll() {
     this.client.rpop(this.queue)
       .then( (job) => {
-        console.log('got job', JSON.parse(job))
-        if(job) this.getEmAll();
+        debug('got job', JSON.stringify(job))
+        if(!job){
+          debug('no more work to do!');
+          return;
+        } this.getEmAll();
       })
   }
 
@@ -39,16 +45,16 @@ class PFMWorkerMaster {
 
 const options = doCliStuff();
 
-const master = new PFMWorkerMaster(options.subscribeTopic, options.queue);
+const master = new PFMWorkerMaster(options.beginTopic, options.queue);
 
 master.start();
 
 function doCliStuff() {
   var cliParser = dashdash.createParser({options: [
     {
-      names: ['subscribe-topic', 's'],
+      names: ['begin-topic', 'b'],
       type: 'string',
-      help: 'the name of the topic to subscribe'
+      help: 'The name of the topic that indicates work is to be done'
     },
     {
       names: ['queue', 'q'],
@@ -65,7 +71,7 @@ function doCliStuff() {
     die();
   }
 
-  if(!options.subscribe_topic || !options.queue) {
+  if(!options.begin_topic || !options.queue) {
     die();
   }
 
@@ -75,5 +81,5 @@ function doCliStuff() {
     process.exit(-1);
   }
 
-  return {queue: options.queue, subscribeTopic: options.subscribe_topic};
+  return {queue: options.queue, beginTopic: options.begin_topic};
 }
--- worker.js
+++ worker.js
@@ -1,18 +1,80 @@
-var Redis = require('ioredis');
-var uuid = require('uuid')
+'use strict'
+var _         = require('lodash')
+var Redis     = require('ioredis');
+var dashdash  = require('dashdash');
 
-var emitterClient = new Redis();
-var masterClient = new Redis();
-var workerClient = new Redis();
+class PFMWorkerMaster {
+  constructor(emitTopic, queue) {
+      this.emitTopic = emitTopic;
+      this.queue = queue;
+  }
 
-masterClient.subscribe('point', function(){
-  console.log('subscribed to point');
-  masterClient.on('message', function(channel, message){
-      console.log('got', message);
-  });
+  start() {
+    console.log('starting up');
+    this.client = new Redis();
+    this.subscriber = new Redis();
+    this.subscriber.subscribe(this.emitTopic);
+    this.subscriber.on('message', (channel, message) => this.fillQueue(message));
+  }
 
-  var data = uuid.v4()
-  console.log(`emitting to point: ${data}`)
-  emitterClient.publish('point', data)
+  fillQueue (message) {
+    const jobs = JSON.parse(message);
 
-})
+    console.log(`putting ${jobs.length} jobs into ${this.queue}`);
+    this.client.lpush(this.queue, _.map(jobs, JSON.stringify)
+      .then( ()=> this.getEmAll() )
+  }
+
+  getEmAll() {
+    this.client.rpop(this.queue)
+      .then( (job) => {
+        console.log('got job', JSON.stringify(job))
+        if(job) this.getEmAll();
+      })
+  }
+
+  queueJob (job) {
+      console.log(`I was going to queue up ${job}, but I didn't.`);
+  }
+}
+
+const options = doCliStuff();
+
+const master = new PFMWorkerMaster(options.emitTopic, options.queue);
+
+master.start();
+
+function doCliStuff() {
+  var cliParser = dashdash.createParser({options: [
+    {
+      names: ['finished-topic', 'f'],
+      type: 'string',
+      help: 'the name of the topic to emit when work is finished (optional)'
+    },
+    {
+      names: ['queue', 'q'],
+      type: 'string',
+      help: 'the name of the work queue to fill'
+    }
+  ]});
+
+  var options = {}
+  try {
+    options = cliParser.parse(process.argv);
+  }
+  catch(error) {
+    die();
+  }
+
+  if(!options.queue) {
+    die();
+  }
+
+  function die() {
+    console.error("You did something wrong.")
+    console.error(cliParser.help());
+    process.exit(-1);
+  }
+
+  return {queue: options.queue, finishedTopic: options.finished_topic};
+}
