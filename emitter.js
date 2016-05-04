41ad83e23f7d651bc3c90fff2500fdcdb58244f9
redaphid <redaphid@redaphisMacBook.lan>
2016-05-03 21:32:30 -0700

    maybe the emitter does something

--- emitter.js
+++ emitter.js
@@ -1,63 +1,60 @@
+'use strict'
 var Redis    = require('ioredis');
 var uuid     = require('uuid');
 var dashdash = require('dashdash');
 
-const die = () => {
-  console.error("You did something wrong.")
-  console.error(cliParser.help());
-  process.exit(-1);
-}
-
-var cliParser = dashdash.createParser({options: [
-  {
-    names: ['emit-topic', 'e'],
-    type: 'string',
-    help: 'the name of the topic to emit'
-  },
-  {
-    names: ['file', 'f'],
-    type: 'string',
-    help: 'a file containing data to process'
+class PFMEmitter {
+  constructor(emitTopic) {
+      this.emitTopic = emitTopic;
+      this.client = new Redis();
   }
-]});
 
-var options = {}
-try {
-  options = cliParser.parse(process.argv);
-}
-catch(error) {
-  die();
+  emit(data) {
+    this.client.publish(this.emitTopic, JSON.stringify(data))
+      .then( () => this.client.quit())
+  }
+  
 }
 
-if(!options.emit_topic || !options.file) {
-  die();
+const options = doCliStuff();
+
+const emitter = new PFMEmitter(options.emitTopic);
+
+emitter.emit(options.data);
+
+function doCliStuff() {
+  var cliParser = dashdash.createParser({options: [
+    {
+      names: ['emit-topic', 'e'],
+      type: 'string',
+      help: 'the name of the topic to emit'
+    },
+    {
+      names: ['file', 'f'],
+      type: 'string',
+      help: 'a file containing data to process'
+    }
+  ]});
+
+  var options = {}
+  try {
+    options = cliParser.parse(process.argv);
+  }
+  catch(error) {
+    die();
+  }
+
+  if(!options.emit_topic || !options.file) {
+    die();
+  }
+
+  const data = require(options.file);
+
+  function die() {
+    console.error("You did something wrong.")
+    console.error(cliParser.help());
+    process.exit(-1);
+  }
+
+  return {data, emitTopic: options.emit_topic};
 }
-
-
-//
-//
-// var emitterClient = new Redis();
-//
-//
-// var file = _.last(process.argv)
-//
-// if(!file) {
-//   console.error("Give me a file with data to add to the queue");
-//   process.exit -1
-// }
-// var data = require(file)
-//
-// var emitterClient = new Redis();
-//
-//
-// masterClient.subscribe('point', function(){
-//   console.log('subscribed to point');
-//   masterClient.on('message', function(channel, message){
-//       console.log('got', message);
-//   });
-//
-//   var data = uuid.v4()
-//   console.log(`emitting to point: ${data}`)
-//   emitterClient.publish('point', data)
-//
-// })
